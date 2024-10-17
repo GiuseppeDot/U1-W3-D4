a5c4a34127bdb41d9a19d71547b29789be14ce2e
GiuseppeDot <giuseppegianni190@gmail.com>
2024-10-17 16:58:40 +0200

    agg 2

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const celle = 76
+
+
+const tabellone = function () {
+const tabellone = document.getElementById("tabellone");
+for(let i = 0; i < celle; i++) 
+};
+
+
+//non ho capito come fare le celle....
